979c1a09d6e19d8f8cf8a28177e9eb028e926e27
Alan Denis <88785284+Alan-denis@users.noreply.github.com>
2023-12-15 10:52:47 +0100

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,14 @@ import zio._
 import zio.Console.printLine
 
 object Main extends ZIOAppDefault {
+
+  // Define an effect to ask a question and read user input
+  val askUser: ZIO[Console, IOException, String] =
+    for {
+      _     <- console.putStrLn("What is your location ?")
+      input <- console.getStrLn
+    } yield input
+
   override def run: ZIO[Environment with ZIOAppArgs with Scope, Any, Any] =
-    printLine("Welcome to your first ZIO app!")
+    askUser.flatMap(location => console.putStrLn(s"You are located at $name!")).exitCode
 }
