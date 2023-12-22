5d8304c39167cbbef1f7e3b63271a3f1c04c9c7d
LoganTann <logon313@hotmail.fr>
2023-12-22 10:18:48 +0100

    feat: finish implement #1

--- src/main/scala/fr/scalapompe/controllers/DataController.scala
+++ src/main/scala/fr/scalapompe/controllers/DataController.scala
@@ -5,17 +5,19 @@ import fr.scalapompe.services.FindStationService
 import fr.scalapompe.mocks.StationsApiRecordsMock
 import java.nio.file.Paths
 import zio.stream.ZStream
+import zio.Task
 
 object DataController {
   val routes = Routes(
-    Method.GET / "data" -> handler(getData())
+    Method.GET / "data" -> Handler.fromFunctionZIO(getData),
+    Method.GET / "testParams" -> Handler.fromFunction(testQueryParams)
   )
 
   val relativePath = Paths.get("").toAbsolutePath()
 
   /** GET /data : Retourne le contenu du fichier data.txt en JSON
     */
-  def getData() = {
+  def getData(request: Request): Task[Response] = {
     val file =
       Paths.get(relativePath.toString().concat("\\src\\main\\scala\\data.txt"))
 
@@ -24,5 +26,20 @@ object DataController {
       .runCollect
       .map(bytes => new String(bytes.toArray))
       .map(response => Response.json(response))
+  }
+
+  /** GET /testParams?lat=XXX&lon=XXX : Retourne les paramètres de la requête
+    */
+  def testQueryParams(request: Request) = {
+    val queryParams = request.url.queryParams
+    val lat = queryParams.get("lat")
+    val lon = queryParams.get("lon")
+
+    if (lat.isDefined && lon.isDefined)
+      Response.text(
+        s"Vous avez spécifié les paramètres suivants : lat: ${lat.get}, lon: ${lon.get}"
+      )
+    else
+      Response.text("Spécifiez les paramètres lat et lon");
   }
 }
--- src/main/scala/fr/scalapompe/controllers/SearchController.scala
+++ src/main/scala/fr/scalapompe/controllers/SearchController.scala
@@ -3,14 +3,15 @@ package fr.scalapompe.controllers
 import zio.http._
 import fr.scalapompe.services.FindStationService
 import fr.scalapompe.mocks.StationsApiRecordsMock
+import zio.Task
 
 object SearchController {
   val routes = Routes(
-    Method.GET / "search" -> handler(getSearchResults())
+    Method.GET / "search" -> Handler.fromFunctionZIO(getSearchResults)
   )
 
   /** GET /search : Retourne le résultat de la recherche */
-  def getSearchResults() = {
+  def getSearchResults(request: Request): Task[Response] = {
     FindStationService
       .ComputeQueryResultFromAllStations(StationsApiRecordsMock.value)
       .map(response => Response.json(response))
